854013225f64bfb8a70b547193acb15fe9f56675
Mateusz Serkies <mateusz.serkies@artifexmundi.com>
2019-11-02 22:01:27 +0100

    Added RemoteServices header.

---
+++ Include/RemoteServices/RemoteServices.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <memory>
+
+namespace RemoteServices
+{
+    using byte = unsigned char;
+
+    #define REMOTE_SERVICES_ASSERT(expression) assert(expression)
+
+    #define DECLARE_POINTERS(x)                                 \
+        class x;                                                \
+        using x##SharedPtr = std::shared_ptr<x>;                \
+        using x##ConstSharedPtr = std::shared_ptr<const x>;     \
+        using x##WeakPtr = std::weak_ptr<x>;                    \
+        using x##ConstWeakPtr = std::weak_ptr<const x>;         \
+        using x##UniquePtr = std::unique_ptr<x>;                \
+        using x##ConstUniquePtr = std::unique_ptr<const x>;     \
+
+    DECLARE_POINTERS(IService);
+    DECLARE_POINTERS(IServiceConnection);
+    DECLARE_POINTERS(IServiceFactory);
+    DECLARE_POINTERS(IServices);
+    DECLARE_POINTERS(IServicesController);
+    DECLARE_POINTERS(IServicesBuilder);
+    DECLARE_POINTERS(IServicesBuildersProvider);
+}
