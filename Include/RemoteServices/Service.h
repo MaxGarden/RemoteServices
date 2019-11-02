aec2e2e5f3c2186dd7ca65786fea31f6d65dc873
Mateusz Serkies <mateusz.serkies@artifexmundi.com>
2019-11-02 22:02:33 +0100

    Added Service.

---
+++ Include/RemoteServices/Service.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "DataModels/Services.h"
+
+namespace RemoteServices
+{
+    class IServiceConnection
+    {
+    public:
+        virtual ~IServiceConnection() = default;
+
+        virtual bool Send(ServicePayload&& payload) = 0;
+    };
+
+    class IService
+    {
+    public:
+        virtual ~IService() = default;
+
+        virtual bool Initialize() = 0;
+
+        virtual void OnBind(const IServiceConnectionSharedPtr& connection) = 0;
+        virtual void OnUnbind(const IServiceConnectionSharedPtr& connection) = 0;
+
+        virtual void Finalize() = 0;
+
+        virtual void OnReceived(const std::vector<byte>& payload) = 0;
+    };
+
+    class IServiceFactory
+    {
+    public:
+        virtual ~IServiceFactory() = default;
+
+        virtual const std::string& GetServiceName() const noexcept = 0;
+        virtual IServiceUniquePtr Create() = 0;
+    };
+}
